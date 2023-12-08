27f059c52374fbcc6d12b887a9baeddd94398015
beeb <703631+beeb@users.noreply.github.com>
2023-12-08 09:10:01 +0100

    feat: day 8

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,25 +1,165 @@
-use nom::IResult;
+use std::collections::HashMap;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alphanumeric1, char, line_ending},
+    combinator::map,
+    multi::{many1, separated_list0},
+    sequence::{preceded, separated_pair, terminated},
+    IResult,
+};
+use num::Integer;
 
 use crate::days::Day;
 
 pub struct Day08;
 
-impl Day for Day08 {
-    type Input = String;
+#[derive(Debug)]
+pub enum Dir {
+    Left,
+    Right,
+}
 
-    fn parse(_input: &str) -> IResult<&str, Self::Input> {
-        unimplemented!("parser")
+#[derive(Debug)]
+pub struct Node {
+    pub name: String,
+    pub left: String,
+    pub right: String,
+}
+
+/// Count how many steps from a start node until the end condition is met
+///
+/// For part 1, end condition is that the node is named 'ZZZ'. For part 2, any node that ends with 'Z'
+fn count_steps(
+    instructions: &[Dir],
+    nodes: &HashMap<String, Node>,
+    start_node: &Node,
+    part2: bool,
+) -> usize {
+    let mut count = 0;
+    let mut node = start_node;
+    for i in instructions.iter().cycle() {
+        count += 1;
+        node = match i {
+            Dir::Left => nodes.get(&node.left).unwrap(),
+            Dir::Right => nodes.get(&node.right).unwrap(),
+        };
+        if (!part2 && node.name == "ZZZ") || (part2 && node.name.ends_with('Z')) {
+            break;
+        }
+    }
+    count
+}
+
+fn parse_instructions(input: &str) -> IResult<&str, Vec<Dir>> {
+    many1(map(alt((char('L'), char('R'))), |c| match c {
+        'L' => Dir::Left,
+        'R' => Dir::Right,
+        _ => unimplemented!(),
+    }))(input)
+}
+
+fn parse_node(input: &str) -> IResult<&str, Node> {
+    map(
+        separated_pair(
+            alphanumeric1::<&str, _>,
+            tag(" = "),
+            preceded(
+                char('('),
+                terminated(
+                    separated_pair(alphanumeric1, tag(", "), alphanumeric1),
+                    char(')'),
+                ),
+            ),
+        ),
+        |(name, (left, right))| Node {
+            name: name.to_string(),
+            left: left.to_string(),
+            right: right.to_string(),
+        },
+    )(input)
+}
+
+impl Day for Day08 {
+    type Input = (Vec<Dir>, HashMap<String, Node>);
+
+    fn parse(input: &str) -> IResult<&str, Self::Input> {
+        map(
+            separated_pair(
+                parse_instructions,
+                tag("\n\n"),
+                separated_list0(line_ending, parse_node),
+            ),
+            |(instructions, nodes)| {
+                (
+                    instructions,
+                    nodes.into_iter().map(|n| (n.name.clone(), n)).collect(),
+                )
+            },
+        )(input)
     }
 
     type Output1 = usize;
 
-    fn part_1(_input: &Self::Input) -> Self::Output1 {
-        unimplemented!("part_1")
+    /// Part 1 took 407.9µs
+    fn part_1(input: &Self::Input) -> Self::Output1 {
+        // We stored all the nodes in a HashMap with the node name as the key
+        let (instructions, nodes) = input;
+        // Use the "AAA" node as a starting point
+        count_steps(instructions, nodes, nodes.get("AAA").unwrap(), false)
     }
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    /// Part 2 took 2.4632ms
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        let (instructions, nodes) = input;
+        // Find all the starting nodes (ending with 'A') and count how long until we reach and end node for each
+        let counts: Vec<usize> = nodes
+            .iter()
+            .filter_map(|(k, v)| k.ends_with('A').then_some(v))
+            .map(|n| count_steps(instructions, nodes, n, true))
+            .collect();
+        // Get the lowest common multiplier between all the counts
+        counts.into_iter().reduce(|acc, e| acc.lcm(&e)).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let input = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+        let parsed = Day08::parse(input).unwrap().1;
+        assert_eq!(Day08::part_1(&parsed), 2);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        let parsed = Day08::parse(input).unwrap().1;
+        assert_eq!(Day08::part_2(&parsed), 6);
     }
 }

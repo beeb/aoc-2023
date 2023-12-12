e1a5c408c3c2a3d71c67d40194da0b42d76fcc2d
beeb <703631+beeb@users.noreply.github.com>
2023-12-12 16:47:15 +0100

    feat: day 12

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::{repeat_n, Itertools};
 use nom::{
     branch::alt,
@@ -12,7 +14,7 @@ use crate::days::Day;
 
 pub struct Day12;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub enum HotSpring {
     Working,
     Broken,
@@ -38,45 +40,102 @@ fn parse_groups(input: &str) -> IResult<&str, Vec<usize>> {
     separated_list1(char(','), map(u64, |n| n as usize))(input)
 }
 
-pub fn is_solution_valid(springs: &[HotSpring], groups: &[usize]) -> bool {
-    // first check that the numbers match
-    if groups.iter().sum::<usize>()
-        != springs
-            .iter()
-            .filter(|s| matches!(s, HotSpring::Broken))
-            .count()
-    {
-        return false;
+/// How many solutions are there, considering a subset of the hotsprings and groups
+///
+/// Solved recursively and caching results
+fn count_solutions<'a>(
+    cache: &mut HashMap<(&'a [HotSpring], &'a [usize]), usize>,
+    springs: &'a [HotSpring],
+    groups: &'a [usize],
+    first_is_broken: bool,
+) -> usize {
+    if springs.is_empty() && groups.is_empty() {
+        return 1;
+    } else if springs.is_empty() {
+        // not a valid solution
+        return 0;
     }
-    // then check that the groups match
-    let effective_groups = springs
-        .split(|s| matches!(s, HotSpring::Working))
-        .filter_map(|g| if g.is_empty() { None } else { Some(g.len()) })
-        .collect_vec();
-    effective_groups == groups
-}
 
-pub fn find_all_solutions(springs: &[HotSpring], groups: &[usize]) -> Vec<Vec<HotSpring>> {
-    let unknowns = springs
-        .iter()
-        .filter(|s| matches!(s, HotSpring::Unknown))
-        .count();
-    let combinations = repeat_n([HotSpring::Working, HotSpring::Broken], unknowns)
-        .multi_cartesian_product()
-        .collect_vec();
-    combinations
-        .into_iter()
-        .map(|mut knowns| {
-            springs
+    // check cache and return if possible
+    if let Some(res) = cache.get(&(springs, groups)) {
+        return *res;
+    }
+
+    // check the first item in the subset of springs
+    match (springs.first(), first_is_broken) {
+        (Some(HotSpring::Working), _) => {
+            // if first item is working, then we simply continue looking starting at the next spring
+            let res = count_solutions(cache, &springs[1..], groups, false);
+            cache.insert((springs, groups), res);
+            res
+        }
+        (Some(HotSpring::Broken), _) | (Some(HotSpring::Unknown), true) => {
+            // if the first item is broken (or is unknown but we want to consider it as broken)
+            if groups.is_empty() {
+                // if we have no more groups to "assign", we have no more options to consider
+                cache.insert((springs, groups), 0);
+                return 0;
+            }
+
+            // length of the first group to be assigned
+            let first_group_length = *groups.first().unwrap();
+
+            // number of contiguous positions we have at our disposal to fit this group (broken or unknown)
+            let maybe_broken = springs
                 .iter()
-                .map(|s| match s {
-                    HotSpring::Unknown => knowns.pop().unwrap_or(s.clone()),
-                    _ => s.clone(),
-                })
-                .collect_vec()
-        })
-        .filter(|row| is_solution_valid(row, groups))
-        .collect()
+                .take_while(|s| !matches!(s, HotSpring::Working))
+                .count();
+            if maybe_broken < first_group_length {
+                // first group wouldn't fit first space, dead end
+                cache.insert((springs, groups), 0);
+                return 0;
+            }
+            // group would fit, let's investigate
+
+            // rest after we assign the first group to the start of the space
+            let rest = &springs[first_group_length..];
+            // let's consider the first item of that rest
+            match rest.first() {
+                None => {
+                    // We are at the end of the row, there is nothing left after we assign the group.
+                    // Let's consider the group assigned and count the options for the remaining groups.
+                    // This will either be 0 or 1 depending on if all the groups have been assigned or not.
+                    // If we have assigned the last group, then the call below will return 1.
+                    // If we still ahve leftover groups but no more springs, then this is not a valid solution and the
+                    // call below will return 0.
+                    let res = count_solutions(cache, rest, &groups[1..], false);
+                    cache.insert((springs, groups), res);
+                    res
+                }
+                Some(HotSpring::Broken) => {
+                    // Not allowed, there should be a gap after the group
+                    cache.insert((springs, groups), 0);
+                    0
+                }
+                Some(HotSpring::Working | HotSpring::Unknown) => {
+                    // if unknown, needs to be a working spring anyway (due to us assigning a group of broken springs),
+                    // so we can skip the first item
+                    let res = count_solutions(cache, &rest[1..], &groups[1..], false);
+                    cache.insert((springs, groups), res);
+                    res
+                }
+            }
+        }
+        (Some(HotSpring::Unknown), false) => {
+            // If the first item is unknown, it might be working or broken.
+            // Let's count the solutions when it's working (same as above)
+            let res_if_working = count_solutions(cache, &springs[1..], groups, false);
+            // In case the first item would be broken, we override the first element's type by passing `true` as the
+            // last param, we will consider it just like a broken spring (same as above).
+            let res_if_broken = count_solutions(cache, springs, groups, true); // consider the first one broken
+
+            // The number of valid options is the sum of those cases
+            let res = res_if_working + res_if_broken;
+            cache.insert((springs, groups), res);
+            res
+        }
+        (None, _) => unreachable!(),
+    }
 }
 
 impl Day for Day12 {
@@ -94,17 +153,45 @@ impl Day for Day12 {
 
     type Output1 = usize;
 
+    /// Part 1 took 5.584ms
     fn part_1(input: &Self::Input) -> Self::Output1 {
         input
             .iter()
-            .map(|row| find_all_solutions(&row.springs, &row.groups).len())
+            .map(|row| {
+                let mut cache = HashMap::new();
+                count_solutions(&mut cache, &row.springs, &row.groups, false)
+            })
             .sum()
     }
 
     type Output2 = usize;
 
-    fn part_2(_input: &Self::Input) -> Self::Output2 {
-        unimplemented!("part_2")
+    /// Part 2 took 220.059002ms
+    fn part_2(input: &Self::Input) -> Self::Output2 {
+        input
+            .iter()
+            .map(|row| {
+                let mut cache = HashMap::new();
+                let mut springs = repeat_n(row.springs.iter().copied().collect_vec(), 5)
+                    .map(|mut a| {
+                        // add the separator (a "unknown" spring)
+                        a.push(HotSpring::Unknown);
+                        a
+                    })
+                    .collect_vec()
+                    .concat();
+                // drop the last unknown separator
+                springs.pop();
+                let groups = row
+                    .groups
+                    .iter()
+                    .cycle()
+                    .take(row.groups.len() * 5)
+                    .copied()
+                    .collect_vec();
+                count_solutions(&mut cache, &springs, &groups, false)
+            })
+            .sum()
     }
 }
 
@@ -126,30 +213,8 @@ mod tests {
     }
 
     #[test]
-    fn test_is_solutions_valid() {
-        assert!(is_solution_valid(
-            &[
-                HotSpring::Broken,
-                HotSpring::Working,
-                HotSpring::Broken,
-                HotSpring::Working,
-                HotSpring::Broken,
-                HotSpring::Broken,
-                HotSpring::Broken
-            ],
-            &[1, 1, 3]
-        ));
-        assert!(!is_solution_valid(
-            &[
-                HotSpring::Working,
-                HotSpring::Broken,
-                HotSpring::Broken,
-                HotSpring::Working,
-                HotSpring::Broken,
-                HotSpring::Broken,
-                HotSpring::Broken
-            ],
-            &[1, 1, 3]
-        ));
+    fn test_part2() {
+        let parsed = Day12::parse(INPUT).unwrap().1;
+        assert_eq!(Day12::part_2(&parsed), 525_152);
     }
 }
